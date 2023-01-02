720ffb7d22736387af29e320cd786531f0fb5952
personal <shaneel_kumar@live.com>
2023-01-02 21:11:49 +1300

    add material ui and style nav bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from '@mui/material/CssBaseline'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Dashboard from './pages/Dashboard'
@@ -6,15 +7,16 @@ import Instructions from './pages/Instructions'
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <Dashboard /> } />
-          <Route path='/instructions' element={ <Instructions /> } />
-        </Routes>
-        </BrowserRouter>
-      </Layout>
-
+      <CssBaseline>
+        <Layout>
+          <BrowserRouter>
+          <Routes>
+            <Route path='/' element={ <Dashboard /> } />
+            <Route path='/instructions' element={ <Instructions /> } />
+          </Routes>
+          </BrowserRouter>
+        </Layout>
+      </CssBaseline>
     </div>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,27 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu'
+import { Box } from "@mui/system";
+
 function Nav() {
   return (
-    <nav>Navbar</nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" color='inherit' aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component='div' sx={{ flexGrow: 1 }}>At What Cost</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
+
 export default Nav
+
+const containerStyle = {
+  width: '100vw',
+  height: 60,
+  background: '#666'
+}
