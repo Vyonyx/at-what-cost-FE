f0d9f709bc91305b1f82847de5a25bdee37ff1cc
personal <shaneel_kumar@live.com>
2023-01-02 16:55:09 +1300

    add react router and add main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Instructions from './pages/Instructions'
 
 function App() {
   return (
     <div className="App">
-      <h1>At What Cost</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Dashboard /> } />
+        <Route path='/instructions' element={ <Instructions /> } />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,6 @@
+function Dashboard() {
+  return (
+    <div>Analysis</div>
+  )
+}
+export default Dashboard
