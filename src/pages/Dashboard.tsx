f4a324a84e2038c789d5290e0ede7877f282fb54
personal <shaneel_kumar@live.com>
2023-01-04 01:03:36 +1300

    add file upload and clear buttons to bottom of transaction list

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
-import { Box, Grid, List, Typography } from "@mui/material"
+import { Box, Button, Grid, List, Typography } from "@mui/material"
 import { Container } from "@mui/system"
 import TransactionListItem from "../components/TransactionListItem"
 
 function Dashboard() {
   return (
     <Grid container columnSpacing={6} sx={{height:'100%', paddingBottom:6, background:'primary.main'}}>
-      <Grid item xs={12} md={6} sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-        <Typography variant="h6" mt={6} mb={2}>
+      <Grid item xs={12} md={6} sx={{display:'flex', flexDirection:'column', alignItems:'center', gap:'1rem'}}>
+        <Typography variant="h6" mt={6}>
           Transactions
         </Typography>
         <Box sx={sectionStyle}>
@@ -17,6 +17,52 @@ function Dashboard() {
             <TransactionListItem transaction="Two Degrees" category='Communication' />
             <TransactionListItem transaction="Mc Donalds" category='Food & Drink' />
           </List>
+        </Box>
+        <Box sx={{display:'flex', gap:'1rem'}}>
+          <Button
+            sx={{
+              padding: '0.5rem 3.25rem',
+              color:'background.default',
+              backgroundColor:'primary.main',
+              '&:hover': {
+                color: 'background.default',
+                backgroundColor: 'primary.main',
+              }
+            }}
+            onClick={() => {
+              const input = document.getElementById('csvUpload')
+              if (input) input.click()
+            }}
+          >
+            <input
+              id='csvUpload'
+              type='file'
+              accept='.csv'
+              style={{display:'none'}}
+            />
+            Upload
+          </Button>
+          <Button
+            disabled
+            sx={{
+              padding: '0.5rem 3.25rem',
+              color:'background.default',
+              backgroundColor:'primary.main',
+              '&:hover': {
+                color: 'background.default',
+                backgroundColor: 'primary.main',
+              },
+              '&:disabled': {
+                color: 'primary.main',
+                backgroundColor: 'background.default',
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderColor: 'primary.main'
+              }
+            }}
+          >
+            Clear
+          </Button>
         </Box>
       </Grid>
       <Grid item xs={12} md={6} sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
@@ -54,7 +100,7 @@ const subSectionStyle = {
 
 const sectionStyle = {
   height: '100%',
-  maxHeight: '600px',
+  maxHeight: '800px',
   overflowY: 'scroll',
   width: '100%',
   borderRadius: '1rem',
