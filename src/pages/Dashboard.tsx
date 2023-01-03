29dcc31a44ce94b2147062848f892a87f221db24
personal <shaneel_kumar@live.com>
2023-01-03 16:18:20 +1300

    add cost breakdown sub components that will contain graph and cost list

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import { Container } from "@mui/system"
 
 function Dashboard() {
   return (
-    <Grid container columnSpacing={10} sx={{height:'100%', paddingBottom:6, background:'primary.main'}}>
+    <Grid container columnSpacing={6} sx={{height:'100%', paddingBottom:6, background:'primary.main'}}>
       <Grid item xs={12} md={6} sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
         <Typography variant="h6" mt={6} mb={2}>
           Transactions
@@ -16,19 +16,38 @@ function Dashboard() {
       <Typography variant="h6" mt={6} mb={2}>
           Cost Breakdown
         </Typography>
-        <Box sx={sectionStyle}>
-          Box
-        </Box>
+        <Container component='div' sx={graphContainerStyle}>
+          <Box sx={subSectionStyle}>
+            Box
+          </Box>
+          <Box sx={subSectionStyle}>
+            Box
+          </Box>
+        </Container>
       </Grid>
     </Grid>
   )
 }
 export default Dashboard
 
+const graphContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap:'1rem',
+  height: '100%'
+}
+
+const subSectionStyle = {
+  flexGrow: 1,
+  width: '100%',
+  borderRadius: '1rem',
+  padding: '2rem',
+  background: '#F2F2F2'
+}
+
 const sectionStyle = {
   height: '100%',
   width: '100%',
-  border: '1px solid black',
   borderRadius: '1rem',
   padding: '2rem',
   background: '#F2F2F2'
