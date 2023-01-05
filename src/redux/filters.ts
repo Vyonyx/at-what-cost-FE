abbaac7488ce793297ca1f0db48d2ce2f79197ff
personal <shaneel_kumar@live.com>
2023-01-05 16:57:02 +1300

    add filter toggle to redux

--- src/redux/filters.ts
+++ src/redux/filters.ts
@@ -1,34 +1,45 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: Filter[] = [
-  {
-    id: 1,
-    transaction: 'BP',
-    category: 'Vehicle'
-  },
-  {
-    id: 2,
-    transaction: 'Raglan Roast',
-    category: 'Food & Drink'
-  },
-]
+type InitialState = {
+  isToggled: boolean;
+  list: Filter[];
+}
+
+const initialState: InitialState = {
+  isToggled: false,
+  list: [
+    {
+      id: 1,
+      transaction: 'BP',
+      category: 'Vehicle'
+    },
+    {
+      id: 2,
+      transaction: 'Raglan Roast',
+      category: 'Food & Drink'
+    },
+  ]
+}
 
 export const filtersSlice = createSlice({
   name: 'filters',
   initialState,
   reducers: {
-    add: (state, action: PayloadAction<Filter>) => {state.push(action.payload)},
+    add: (state, action: PayloadAction<Filter>) => {state.list.push(action.payload)},
     edit: (state, action: PayloadAction<Filter>) => {
       const { id } = action.payload
-      return state.map(filter => {
+      const list = state.list.map(filter => {
         if (filter.id !== id) return filter
         return action.payload
       })
+      return {...state, list}
     },
     delete: (state, action: PayloadAction<number>) => {
       const id = action.payload
-      return state.filter(filter => filter.id !== id)
-    }
+      const list = state.list.filter(filter => filter.id !== id)
+      return {...state, list}
+    },
+    toggle: (state) => {state.isToggled = !state.isToggled}
   }
 })
 
