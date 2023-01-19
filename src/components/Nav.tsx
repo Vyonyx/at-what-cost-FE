7f3738994db0c94a9d372dd98c9ef51eb3c64e16
personal <shaneel_kumar@live.com>
2023-01-19 15:02:43 +1300

    update navbar component to display user details if logged in

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,78 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu'
-import { useAuth0 } from '@auth0/auth0-react'
-import LogoutButton from "./LogoutButton"
-import SignUpButton from "./SignUpButton"
-import SignInButton from "./SignInButton"
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import LogoutButton from "./LogoutButton";
+import SignUpButton from "./SignUpButton";
+import SignInButton from "./SignInButton";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
-  const { isAuthenticated, user } = useAuth0()
-  
+  const user = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (path: string = "") => {
+    setAnchorEl(null);
+    if (path) navigate(path);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar sx={toolbarStyle}>
-        <IconButton size="large" color='inherit' aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component='div' sx={{ flexGrow: 1 }}>At What Cost</Typography>
-        {isAuthenticated ? (
+        <>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={() => handleClose()}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            <MenuItem onClick={() => handleClose("/")}>Home</MenuItem>
+            {user.token && (
+              <>
+                <MenuItem onClick={() => handleClose("tool")}>
+                  Dashboard
+                </MenuItem>
+              </>
+            )}
+          </Menu>
+        </>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          At What Cost
+        </Typography>
+        {user?.token ? (
           <>
-            <Typography mr={4}>{user?.name}</Typography>
+            <Typography mr={4}>{user?.name.toLowerCase()}</Typography>
             <LogoutButton />
           </>
         ) : (
@@ -28,12 +83,12 @@ function Nav() {
         )}
       </Toolbar>
     </AppBar>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
 
 const toolbarStyle = {
-  backgroundColor: 'background.default',
-  color: '#F2F2F2',
-}
+  backgroundColor: "background.default",
+  color: "#F2F2F2",
+};
